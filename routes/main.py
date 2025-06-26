a314a80c7de45251f1d80663fa751da1e80a5c57
David Portillo <a1123150055@utch.edu.mx>
2025-06-26 07:33:53 -0600

    - Add changes

--- routes/main.py
+++ routes/main.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify, render_template, Blueprint
+from flask import request, jsonify, render_template, Blueprint, url_for, redirect
 from db import mongodb
 from models import local_scorer, ai_selector
 from utils import clean_prompt
@@ -10,7 +10,7 @@ from collections import Counter
 import math
 from config import settings
 from bson import ObjectId
-from pymongo import DESCENDING
+from pymongo import DESCENDING, errors
 from datetime import datetime
 import pytz
 
@@ -19,7 +19,7 @@ bp = Blueprint("main_routes", "__name__")
 
 @bp.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("main.html")
 
 @bp.route("/chat")
 def chat():
@@ -27,7 +27,7 @@ def chat():
     if not chat_id:
         return render_template("not_found.html")
     
-    return render_template("index.html", chat_id)
+    return render_template("index.html", chat_id=chat_id)
 
 @bp.route("/get_chat", methods=["POST"])
 def get_chat():
@@ -49,6 +49,8 @@ def get_chat():
             "message": "No chat was found"
         }), 404
 
+    result["_id"] = str(result.get("_id", ""))
+
     return jsonify({
         "status": "successful",
         "message": "Chat was found successfuly",
@@ -58,12 +60,18 @@ def get_chat():
 @bp.route("/get_chats")
 def get_chats():
     colec = mongodb.get_collection("historial")
-    chats = colec.find().sort("fecha_creada", DESCENDING).limit(10)
-    if not chats:
+    cursor = colec.find().sort("fecha_creada", DESCENDING).limit(10)
+    chats = list(cursor)
+
+    if len(chats) == 0:
         return jsonify({
             "status": "error",
             "message": "No chat was found"
         }), 404
+
+    for chat in chats:
+        chat["_id"] = str(chat.get("_id", ""))
+        chat["fecha_creada"] = chat["fecha_creada"].isoformat()
 
     return jsonify({
         "status": "successful",
@@ -84,17 +92,30 @@ def insert_new_chat():
     
     try:
         result = colec.insert_one(query)
-        
-    except:
-        return
-    
-    return
+        chat_id = str(result.inserted_id)
+        return jsonify({
+            "status": "successful",
+            "message": "the chat was inserted succesfuly",
+            "results": chat_id
+        }), 200
+    except errors.DuplicateKeyError:
+        return jsonify({
+            "status": "error",
+            "message": "Duplicate key error"
+        }), 409
+    except errors.PyMongoError as e:
+        print(f"Error inserting a new chat: {e}")
+        return jsonify({
+            "status": "error",
+            "message": "the chat could not be inserted"
+        }), 500
 
 @bp.route("/ask", methods=["POST"])
 def ask():
     data = request.get_json()
     topic = data.get("tema")
     question = data.get("pregunta", "")
+    id = data.get("chat_id")
     to_find = clean_prompt(question).strip()
     
     print("Pregunta original:", question)
@@ -102,7 +123,6 @@ def ask():
     
     colec = mongodb.get_collection(topic)
     
-    # Obtener las mejores 5 respuestas
     docs = list(colec.find(
         {"$text": {"$search": to_find}},
         {
@@ -114,6 +134,13 @@ def ask():
     
     if not docs:
         print("ENTRO AL PRIMER 404")
+        updated = update_chat(id , question, "Lo siento, no encontré una respuesta.", True)
+        if not updated:
+            return jsonify({
+                "status": "error",
+                "respuesta": "No tienes una conexión correcta a tu chat"
+            }), 500
+        
         return jsonify({"respuesta": "Lo siento, no encontré una respuesta."}), 404
     
     print("Estos son los docs: ", docs)
@@ -122,9 +149,22 @@ def ask():
     valid_docs = [doc for doc in docs if doc.get("score", 0) >= min_score]
     
     if not valid_docs:
+        updated = update_chat(id, question, "Lo siento, no encontré una respuesta relevante.", True)
+        if not updated:
+            return jsonify({
+                "status": "error",
+                "respuesta": "No tienes una conexión correcta a tu chat"
+            }), 500
         return jsonify({"respuesta": "Lo siento, no encontré una respuesta relevante."}), 404
     
     if len(valid_docs) == 1:
+        updated = update_chat(id, question, valid_docs[0]["respuesta"], False)
+        if not updated:
+            return jsonify({
+                "status": "error",
+                "respuesta": "No tienes una conexión correcta a tu chat"
+            }), 500
+        
         return jsonify({
             "respuesta": valid_docs[0]["respuesta"],
             "metodo": "unica_respuesta",
@@ -158,6 +198,13 @@ def ask():
     selected_doc = valid_docs[best_index]
 
     print("Se termino usando: ", selection_method)
+
+    updated = update_chat(id, question, valid_docs[0]["respuesta"], False)
+    if not updated:
+        return jsonify({
+            "status": "error",
+            "respuesta": "No tienes una conexión correcta a tu chat"
+        }), 500
     
     return jsonify({
         "respuesta": selected_doc["respuesta"],
@@ -167,3 +214,36 @@ def ask():
         "validas": len(valid_docs),
         "subtema": selected_doc.get("subtema")
     }), 200
+
+
+def update_chat(id, question, answer, error: bool):
+    collection = mongodb.get_collection("historial")
+    chat_id = ObjectId(id)
+    query = {
+        "_id": chat_id
+    }
+    chat = collection.find_one(query)
+    messages = chat["chat"]
+
+    message = {
+        "usuario": question,
+        "bot": answer,
+        "error": error
+    }
+
+    messages.append(message)
+
+    if len(messages) > 25:
+        messages = messages[-25]
+
+    try:
+        collection.update_one(
+            {"_id": chat_id},
+            {"$set": {"chat": messages}}
+        )
+        print("EVERYTHING WENT WELL WITH THE UPDATE")
+        updated = True
+    except:
+        print("BRO, YOU DON'T EVEN KNOW HOW TO MAKE AN UPDATE")
+        updated = False
+    return updated
